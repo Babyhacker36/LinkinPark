6c6ed5d8036be27d86c3f3d2eabfe84fde8cb507
louiscarterjr29@gmail.com <louiscarterjr29@gmail.com>
2020-06-15 20:12:15 -0500

    changes

--- js/script.js
+++ js/script.js
@@ -5,9 +5,27 @@ window.onscroll = function() {
 };
 
 function scrollFunction() {
-  if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 550) {
+  if (document.body.scrollTop > 550 || document.documentElement.scrollTop > 50) {
     document.getElementById("hidden-navbar").style.top = "0";
   } else {
     document.getElementById("hidden-navbar").style.top = "-80px";
   }
 }
+
+
+// const musicPlayer = document.getElementById('musicPlayer')
+
+// musicPlayer.autoplay
+
+
+window.sr = ScrollReveal({
+  reset: true,
+  useDelay: 'always',
+  delay: 200
+});
+
+sr.reveal('#drinks');
+sr.reveal('#bar');
+sr.reveal('#foto');
+sr.reveal('#contact');
+sr.reveal('#footer');
